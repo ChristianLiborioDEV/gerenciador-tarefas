69a49e4089d91cecd2f750ee1f96256168490d94
Christian Liborio <christianliborio.dev@gmail.com>
2026-07-27 18:44:17 -0300

    novas funções

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const tarefas = [
 ];
 
 
+/* -------------------------------------------------------------------------------------------------------------------------------------*/
 //criar Id para tarefa
 const gerarId = () => {
     let count = 4;
@@ -14,34 +15,58 @@ const gerarId = () => {
         return count;
     };
 };
-
 const proximoId = gerarId();
 
+/* -------------------------------------------------------------------------------------------------------------------------------------*/
 // Adicionar nova tarefa
 const adicionarTarefa = (titulo, prioridade) => {
     const novaTarefa = {id: proximoId(), titulo: titulo, prioridade: prioridade, realizada: false, dataCriacao: new Date()}
     tarefas.push(novaTarefa)
     return novaTarefa
 }
-
 adicionarTarefa("Estudar JavaScript", "alta");
 
+/* -------------------------------------------------------------------------------------------------------------------------------------*/
 // Remover tarefas pelo id
 const removerTarefa = (id) => {
     const indice = tarefas.findIndex(a => a.id === id)
     if (indice === -1) return null;
     return tarefas.splice(indice, 1)[0];
 }
+//removerTarefa(15)
 
-removerTarefa(3)
-//console.log(tarefas);
-
+/* -------------------------------------------------------------------------------------------------------------------------------------*/
 // Marcar tarefa como concluida
 const marcarRealizada = (id) => {
-    const tarefaStatus = tarefas.find(a => a.id === id)
-    return (tarefaAtualizada) => ({...tarefas, realizada: tarefaStatus.realizada? false : true})
+    const tarefaIndice = tarefas.findIndex(a => a.id === id)
+    if (tarefaIndice === -1) return null;
+    tarefas[tarefaIndice] = {...tarefas[tarefaIndice], realizada: !tarefas[tarefaIndice].realizada}
+    return tarefas[tarefaIndice]
+}
+//console.log(marcarRealizada(2));
+
+/* -------------------------------------------------------------------------------------------------------------------------------------*/
+//Filtrar por Status
+const filtrarPorStatus = (feita) => {
+    const tarefaFiltro = tarefas.filter(a => a.realizada === feita)
+    return tarefaFiltro
 }
 
-console.log(tarefas[0].realizada); // false
-marcarRealizada(2);
-console.log(tarefas[0].realizada); // true
+/* -------------------------------------------------------------------------------------------------------------------------------------*/
+//Ordenar a Prioridade
+const ordenarPorPrioridade = () => {
+    const ordem = { alta: 1, media: 2, baixa: 3 };
+    const ordenarTarefas = [...tarefas].sort((a, b) => ordem[a.prioridade] - ordem[b.prioridade])
+    return ordenarTarefas
+}
+//console.log(ordenarPorPrioridade())
+
+/* -------------------------------------------------------------------------------------------------------------------------------------*/
+//Buscar por titulo
+const buscarPorTitulo = (titulo) => {
+    const tarefaTitulo = tarefas.filter(a => a.titulo.toLowerCase().includes(titulo.toLowerCase()))
+    return tarefaTitulo
+}
+
+//console.log(buscarPorTitulo("lavar")); 
+//console.log(buscarPorTitulo("LAVAR")); 
